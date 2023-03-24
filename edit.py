0055990d8b33a528798668dc8993e7d2a4d34ccd
xiplus <huangxuanyuxiplus@gmail.com>
2023-03-24 13:24:50 +0800

    Use batch query to improve performance

--- edit.py
+++ edit.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import argparse
 import json
+import logging
 import os
 import re
+import sys
 from datetime import datetime, timedelta
+from typing import Dict, List
 
-import mwparserfromhell
+from pywikibot.textlib import extract_sections
 
 os.environ['PYWIKIBOT_DIR'] = os.path.dirname(os.path.realpath(__file__))
 import pywikibot
@@ -15,10 +18,17 @@ from config import config_page_name  # pylint: disable=E0611,W0614
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pagename', nargs='?')
-parser.add_argument('--debug', action='store_true')
-parser.set_defaults(debug=False)
+parser.add_argument('-c', '--confirm', action='store_true')
+parser.add_argument('-d', '--debug', action='store_const', dest='loglevel', const=logging.DEBUG, default=logging.INFO)
 args = parser.parse_args()
-pywikibot.log(args)
+
+logger = logging.getLogger('archive_ar')
+formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(formatter)
+logger.addHandler(stdout_handler)
+logger.setLevel(args.loglevel)
+logger.debug('args: %s', args)
 
 site = pywikibot.Site()
 site.login()
@@ -26,33 +36,37 @@ site.login()
 config_page = pywikibot.Page(site, config_page_name)
 cfg = config_page.text
 cfg = json.loads(cfg)
-if args.debug:
-    print(json.dumps(cfg, indent=4, ensure_ascii=False))
+logger.debug('config: %s', json.dumps(cfg, indent=4, ensure_ascii=False))
 
 if not cfg['enable']:
     print('disabled')
     exit()
 
 
-def converttitle(title):
-    oldtitle = title
-    r = Request(site=site, parameters={
-        'action': 'query',
-        'titles': title,
-        'redirects': 1,
-        'converttitles': 1
-    })
-    data = r.submit()
-    title = list(data['query']['pages'].values())[0]['title']
+normalized_titles = dict()
+converted_titles = dict()
+redirect_titles = dict()
+
+
+def check_title(old_title):
     mode = []
-    if 'redirects' in data['query']:  # 重定向
-        mode.append('redirects')
-    if 'converted' in data['query']:  # 繁簡轉換
-        mode.append('converted')
-    if 'normalized' in data['query']:  # 命名空間
+    new_title = old_title
+    if new_title[0] == ':':
+        new_title = new_title[1:]
+
+    # 順序不得更改
+    if new_title in normalized_titles:  # 命名空間等
+        new_title = normalized_titles[new_title]
         mode.append('normalized')
+    if new_title in converted_titles:  # 繁簡轉換
+        mode.append('converted')
+        new_title = redirect_titles[new_title]
+    if new_title in redirect_titles:  # 重定向
+        mode.append('redirects')
+        new_title = redirect_titles[new_title]
+
     if 'redirects' not in mode:
-        page = pywikibot.Page(site, title)
+        page = pywikibot.Page(site, new_title)
         if not page.exists():
             mode.append('vfd_on_source')
         if page.exists() and (page.content_model != 'wikitext'
@@ -60,19 +74,19 @@ def converttitle(title):
                               or re.search(r'{{\s*([vaictumr]fd|Copyvio)', page.text, flags=re.I)):
             mode.append('vfd_on_source')
     else:
-        page = pywikibot.Page(site, oldtitle)
+        page = pywikibot.Page(site, old_title)
         if page.exists() and (page.content_model != 'wikitext'
                               or page.namespace().id == 8
                               or re.search(r'{{\s*([vaictumr]fd|Copyvio)', page.text, flags=re.I)):
             mode.append('vfd_on_source')
-        page = pywikibot.Page(site, title)
+        page = pywikibot.Page(site, new_title)
         if page.exists() and (page.content_model != 'wikitext'
                               or page.namespace().id == 8
                               or re.search(r'{{\s*([vaictumr]fd|Copyvio)', page.text, flags=re.I)):
             mode.append('vfd_on_target')
     if 'vfd_on_source' not in mode and 'vfd_on_target' not in mode:
         mode.append('no_vfd')
-    return {'title': title, 'mode': mode}
+    return {'title': new_title, 'mode': mode}
 
 
 def appendComment(text, mode):
@@ -82,25 +96,20 @@ def appendComment(text, mode):
             comment = []
             if 'redirects' in mode and isinstance(cfg['comment_fix']['redirects'], str):
                 comment.append(cfg['comment_fix']['redirects'])
-                if args.debug:
-                    print('\tcomment_fix - redirects')
+                logger.debug('\tcomment_fix - redirects')
             if 'converted' in mode and isinstance(cfg['comment_fix']['converted'], str):
                 comment.append(cfg['comment_fix']['converted'])
-                if args.debug:
-                    print('\tcomment_fix - converted')
+                logger.debug('\tcomment_fix - converted')
             if 'normalized' in mode and isinstance(cfg['comment_fix']['normalized'], str):
                 comment.append(cfg['comment_fix']['normalized'])
-                if args.debug:
-                    print('\tcomment_fix - normalized')
+                logger.debug('\tcomment_fix - normalized')
             if len(comment) > 0:
                 append_text.append(cfg['comment_fix']['main'].format(
                     ''.join(comment)))
-                if args.debug:
-                    print('\tcomment_fix - redirects')
+                logger.debug('\tcomment_fix - redirects')
         if 'no_vfd' in mode:
             append_text.append(cfg['comment_vfd'])
-            if args.debug:
-                print('\tcomment_vfd')
+            logger.debug('\tcomment_vfd')
         if len(append_text) > 0:
             text = text.strip()
             append_text = '\n'.join(append_text)
@@ -109,8 +118,7 @@ def appendComment(text, mode):
                 temp = text.split(hr)
                 text = hr.join(temp[:-1]) + '\n' + append_text + hr + temp[-1]
             else:
-                text += '\n' + append_text
-            text += '\n\n'
+                text += '\n' + append_text + '\n'
     return text
 
 
@@ -128,154 +136,144 @@ def fix(pagename):
     if re.search(r'\d{4}/\d{2}/\d{2}', pagename):
         pagename = 'Wikipedia:頁面存廢討論/記錄/' + pagename
 
-    if args.debug:
-        print('-' * 50)
-        print('running for ' + pagename)
+    logger.debug('-' * 50)
+    logger.info('running for ' + pagename)
 
     afdpage = pywikibot.Page(site, pagename)
     text = afdpage.text
 
-    wikicode = mwparserfromhell.parse(text)
+    header, threads, footer = extract_sections(text, site)
 
-    changes = []
-    for secid, section in enumerate(list(wikicode.get_sections())):
-        if secid == 0:
-            continue
-        title = str(section.get(0).title).strip()
-        if args.debug:
-            print(secid, title)
-        if re.search(r'{{\s*(delh|TalkendH)\s*(\||}})', str(section), re.IGNORECASE) is not None:
-            if args.debug:
-                print('  closed, skip')
+    section_titles: Dict[int, List[str]] = dict()
+    for sec_id, section in enumerate(threads):
+        if re.search(r'{{\s*(delh|TalkendH)\s*(\||}})', section.content, re.IGNORECASE) is not None:
+            logger.debug('%s closed, skip', section.title.strip('= '))
             continue
 
-        m = re.search(r'^\[\[([^\]]+)\]\]$', title, re.IGNORECASE)
+        heading = section.title.strip('= ')
+
+        m = re.search(r'^\[\[([^\]]+)\]\]$', heading, re.IGNORECASE)
+        if m:
+            section_titles[sec_id] = [m.group(1)]
+            continue
+
+        m = re.search(r'^(\[\[[^\]]+\]\][、， ])+\[\[[^\]]+\]\]$', heading, re.IGNORECASE)
+        if m:
+            title_list = re.sub(r'\]\][， ]\[\[', ']]、[[', heading).split('、')
+            section_titles[sec_id] = []
+            for title in title_list:
+                section_titles[sec_id].append(title.strip('[]'))
+            continue
+
+        m = re.search(r'^{{al\|((?:[^\]]+\|)+[^\]]+)}}$', heading, re.IGNORECASE)
         if m is not None:
-            title = m.group(1)
-            start = ''
-            if title[0] == ':':
-                start = ':'
-                title = title[1:]
-
-            mode = []
-
-            convert = converttitle(title)
-            if (('redirects' in convert['mode'] and 'vfd_on_target' in convert['mode'])
-                    or ('redirects' not in convert['mode'])):
-                title = convert['title']
-                mode.append('fix')
-            mode += convert['mode']
-            if args.debug:
-                print('    ', mode)
-
-            title = '[[' + start + title + ']]'
-
-            oldtitle = str(section.get(0).title).strip()
-            if oldtitle != title:
-                if oldtitle.replace('_', ' ').replace('&#39;', "'") != title:
-                    section.insert(1, '\n{{formerly|' + oldtitle + '}}')
-                if args.debug:
-                    print('  set new title = ' + title)
-                section.get(0).title = title
-            newtext = appendComment(str(section), mode)
-            changes.append([secid, newtext])
-            continue
-
-        m = re.search(
-            r'^(\[\[[^\]]+\]\][、， ])+\[\[[^\]]+\]\]$', title, re.IGNORECASE)
-        if m is not None:
-            titlelist = re.sub(r'\]\][， ]\[\[', ']]、[[', m.group(0)).split('、')
-            newtitlelist = []
-            mode = []
-            for title in titlelist:
-                if title.startswith('[[') and title.endswith(']]'):
-                    title = title[2:-2]
-
-                    if title[0] == ':':
-                        title = title[1:]
-
-                    convert = converttitle(title)
-                    if (('redirects' in convert['mode'] and 'vfd_on_target' in convert['mode'])
-                            or ('redirects' not in convert['mode'])):
-                        title = convert['title']
-                        mode.append('fix')
-                    mode += convert['mode']
-
-                    newtitlelist.append(title)
-                else:
-                    if args.debug:
-                        print('  wrong title: ' + title)
-                    return
-            newtitlelist = escapeEqualSign(newtitlelist)
-            title = '{{al|' + '|'.join(newtitlelist) + '}}'
-            if str(section.get(0).title) != title:
-                if args.debug:
-                    print('  set new title = ' + title)
-                section.get(0).title = title
-            newtext = appendComment(str(section), mode)
-            changes.append([secid, newtext])
-            continue
-
-        m = re.search(r'^{{al\|([^\]]+\|)+[^\]]+}}$', title, re.IGNORECASE)
-        if m is not None:
-            titlelist = m.group(0)[5:-2].split('|')
-            newtitlelist = []
-            mode = []
-            for title in titlelist:
+            title_list = m.group(1).split('|')
+            section_titles[sec_id] = []
+            for title in title_list:
                 m = re.search(r'^\s*\d+\s*=\s*(.+)$', title)
                 if m:
-                    title = m.group(1)
-
-                if title[0] == ':':
-                    title = title[1:]
-
-                convert = converttitle(title)
-                if (('redirects' in convert['mode'] and 'vfd_on_target' in convert['mode'])
-                        or ('redirects'not in convert['mode'])):
-                    title = convert['title']
-                    mode.append('fix')
-                mode += convert['mode']
-
-                newtitlelist.append(title)
-            newtitlelist = escapeEqualSign(newtitlelist)
-            title = '{{al|' + '|'.join(newtitlelist) + '}}'
-            if str(section.get(0).title) != title:
-                if args.debug:
-                    print('  set new title = ' + title)
-                section.get(0).title = title
-            newtext = appendComment(str(section), mode)
-            changes.append([secid, newtext])
+                    section_titles[sec_id].append(m.group(1))
+                else:
+                    section_titles[sec_id].append(title)
             continue
 
-        if args.debug:
-            print('  unknown format, skip')
+        logger.debug('%s unknown format, skip', heading)
 
-    for change in changes:
-        wikicode = mwparserfromhell.parse(text)
-        sections = list(wikicode.get_sections())
-        secid, newtext = change
-        wikicode.replace(sections[secid], newtext)
-        text = str(wikicode)
+    all_titles = [title for section in section_titles.values() for title in section]  # flatten
+    for i in range(0, len(all_titles), 50):
+        r = Request(site=site, parameters={
+            'action': 'query',
+            'titles': '|'.join(all_titles[i:i + 50]),
+            'redirects': 1,
+            'converttitles': 1,
+            'format': 'json',
+            'formatversion': 2,
+        })
+        data = r.submit()
+        for item in data['query'].get('normalized', []):
+            normalized_titles[item['from']] = item['to']
+        for item in data['query'].get('converted', []):
+            converted_titles[item['from']] = item['to']
+        for item in data['query'].get('redirects', []):
+            redirect_titles[item['from']] = item['to']
 
-    if re.sub(r'\s+', '', afdpage.text) == re.sub(r'\s+', '', text):
-        if args.debug:
-            print('  nothing changed')
+    new_text = header
+    for sec_id, section in enumerate(threads):
+        if sec_id not in section_titles:
+            new_text += section.title + section.content
+            continue
+
+        m = re.search(r'^(=+)', section.title)
+        if m:
+            old_level = m.group(1)
+        else:
+            logger.warning('fail to check section level {}'.format(section.title))
+            new_text += section.title + section.content
+            continue
+
+        new_titles = []
+        mode = []
+        for old_title in section_titles[sec_id]:
+            convert = check_title(old_title)
+            new_title = old_title
+
+            if (('redirects' in convert['mode'] and 'vfd_on_target' in convert['mode'])
+                    or ('redirects' not in convert['mode'])):
+                new_title = convert['title']
+                if old_title[0] == ':':
+                    new_title = ':' + new_title
+            if old_title != new_title:
+                mode.append('fix')
+            mode += convert['mode']
+
+            new_titles.append(new_title)
+
+        logger.debug('%s: %s', section.title.strip('= '), ', '.join(mode))
+        new_titles = escapeEqualSign(new_titles)
+
+        new_heading = ''
+        if len(new_titles) == 1:
+            new_heading = '[[{}]]'.format(new_titles[0])
+        else:
+            new_heading = '{{al|' + '|'.join(new_titles) + '}}'
+        if section.title.strip('= ') != new_heading:
+            logger.info('change heading to %s', section.title.strip('= '), new_heading)
+
+        new_text += '{0} {1} {0}'.format(old_level, new_heading)
+        new_text += section.content
+        new_text = appendComment(new_text, mode)
+
+    new_text += footer
+
+    if re.sub(r'\s+', '', afdpage.text) == re.sub(r'\s+', '', new_text):
+        logger.info('nothing changed')
+        with open('out.txt', 'w', encoding='utf8') as f:
+            f.write(new_text)
         return
 
-    if args.debug:
-        pywikibot.showDiff(afdpage.text, text)
     summary = cfg['summary']
     if args.debug:
-        print(summary)
-    afdpage.text = text
-    afdpage.save(summary=summary, minor=False)
+        pywikibot.showDiff(afdpage.text, new_text)
+        logger.info('summary: %s', summary)
+
+    save = True
+    if args.confirm:
+        save = pywikibot.input_yn('Save changes for main page?', 'Y')
+    if save:
+        logger.info('save changes')
+        afdpage.text = new_text
+        afdpage.save(summary=summary, minor=False)
+    else:
+        with open('out.txt', 'w', encoding='utf8') as f:
+            f.write(new_text)
+        logger.info('skip save')
 
 
 if args.pagename:
     fix(args.pagename)
 else:
     if args.debug:
-        print('run past {} days'.format(cfg['run_past_days']))
+        logger.info('run past %s days', cfg['run_past_days'])
     for delta in range(cfg['run_past_days']):
         rundate = datetime.now() - timedelta(days=delta)
         pagename = rundate.strftime('%Y/%m/%d')
